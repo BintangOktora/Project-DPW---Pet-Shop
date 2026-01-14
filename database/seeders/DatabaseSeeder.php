a3c1a5801b999730336f879c0339853fd2251eb5
Bintang Oktora <bintangoktora40@gmail.com>
2026-01-14 12:54:54 +0700

    modified:   database/seeders/DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Create default admin user
+        User::create([
+            'nama_user' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => bcrypt('admin123'),
+            'no_hp' => '',
         ]);
     }
 }
